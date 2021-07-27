2ffceca3682851dbb338d2fefc90bf14a8590db0
Konstantin Nabokov <kostnab1991@gmail.com>
2021-07-27 18:11:35 +1000

    Add "/posts..." to DefaultController for "index" mapping

--- src/main/java/ru/skillbox/blogenginediploma/controller/DefaultController.java
+++ src/main/java/ru/skillbox/blogenginediploma/controller/DefaultController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DefaultController {
-    @GetMapping
+    @GetMapping({"/", "/posts/**"})
     public String index() {
         return "index";
     }
