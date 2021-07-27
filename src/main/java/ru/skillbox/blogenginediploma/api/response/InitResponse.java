0238cb4ca06ab97a040dd558377645dba5a4e7f8
Konstantin Nabokov <kostnab1991@gmail.com>
2021-07-27 18:19:16 +1000

    Using @ConfigurationProperties for InitResponse

--- src/main/java/ru/skillbox/blogenginediploma/api/response/InitResponse.java
+++ src/main/java/ru/skillbox/blogenginediploma/api/response/InitResponse.java
@@ -3,29 +3,19 @@ package ru.skillbox.blogenginediploma.api.response;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ConfigurationProperties(prefix = "blog")
 public class InitResponse {
-    @Value("${blog.title}")
     private String title;
-
-    @Value("${blog.subtitle}")
     private String subtitle;
-
-    @Value("${blog.phone}")
     private String phone;
-
-    @Value("${blog.email}")
     private String email;
-
-    @Value("${blog.copyright}")
     private String copyright;
-
-    @Value("${blog.copyrightFrom}")
     private String copyrightFrom;
 }
